ac4ab357bd1fa24b976797a118aba15e28f20528
Dan Jackson <dannyjacks2019@gmail.com>
2023-04-16 12:07:56 -0400

    Adds index.html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Personal Hobbies";
 document.querySelector("body").appendChild(h2);
+
+
